31020ab6d15ca3babaef68d577855a317a2663b9
Kayke Adriano <kayke.adrianobdg@gmail.com>
2025-01-15 16:23:38 -0400

    sql

--- bd.sql
+++ bd.sql
@@ -1,1 +1,9 @@
-kkk
+create database Reservas;
+use Reservas;
+
+create table usuario(
+	id_usuario int auto_increment primary key,
+    nome varchar(80),
+    email varchar(90),
+    telefone varchar(14)
+);
