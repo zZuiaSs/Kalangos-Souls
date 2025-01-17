7c73a947b96f54aa85e6a64666b4c0eebee1e98b
Kayke Adriano <kayke.adrianobdg@gmail.com>
2025-01-17 14:15:14 -0400

    deletei o sql

--- ATV/pages/cadastro/index.php
+++ ATV/pages/cadastro/index.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="Pt - Br">
 <head>
-    <meta charset="UTF-8">
+    <meta charset="UTF-8">l
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Cadastro</title>
 </head>
@@ -16,4 +16,4 @@
     </form>    
     
 </body>
-</html>
+</html> 
--- bd.sql
+++
@@ -1,9 +0,0 @@
-create database Reservas;
-use Reservas;
-
-create table usuario(
-	id_usuario int auto_increment primary key,
-    nome varchar(80),
-    email varchar(90),
-    telefone varchar(14)
-);
