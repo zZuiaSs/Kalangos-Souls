e4c9f40404ca41e8aeea59749f5b8b28da31b190
Kayke Adriano <kayke.adrianobdg@gmail.com>
2025-01-17 15:16:34 -0400

    cadastro

--- Pages/Cadastro/index.php
+++ Pages/Cadastro/index.php
@@ -1,19 +1,59 @@
 <!DOCTYPE html>
 <html lang="Pt - Br">
 <head>
-    <meta charset="UTF-8">l
+    <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Cadastro</title>
 </head>
-
 <body>
+        <div class="fundo">
+                        <h1>Cadastro</h1>
+            <form method = "post">
+                <input type="text" name = "inp1" placeholder = "Nome">
+                <input type="text" name = "inp2" placeholder = "E-mail"> 
+                <div class = "bt">
+                    <input type="submit">
+                </div>
+            </form>
+        </div>
 
-    <form action="../../Backend/router/loginRouter.php" method="POST">  
-        <input type="hidden" name="usuarioId" value="<?php echo $usuario["id"]  ?>">
-        <input type="text" name = "nome" value="<?php echo $usuario["nome"] ?>"> 
-        <input type="pasword" name ="senha" value="<?php echo $usario["senha"] ?>">
-        <button type="submit"><?php echo $variavel ?></button>
-    </form>    
-    
+        
+        <style>
+            body {
+                display: flex;
+                justify-content: center;
+                align-items: center;
+                height: 100vh;
+                flex-direction: column;
+            }
+
+            .fundo {
+                display: flex;
+                background-color: black;
+                height: 350px;
+                width: 250px;
+            }
+
+            h1 {
+                display: flex;
+                justify-content: center;
+                align-items: center;
+                color: white;
+            }
+
+            input {
+                display: flex;
+                padding: 2px;
+                margin-bottom: 5px;
+            }
+
+            .bt {
+                display: flex;
+                justify-content: center;
+                align-items: center;
+            }
+
+        </style>
+
 </body>
-</html> 
+</html>
