b50657ae82d212aa54f7cd4fc8029a1875df95e9
Josué <josuealexandre067@gmail.com>
2025-01-17 15:25:50 -0400

    15:25

---
+++ Pages/Login/index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="Pt - Br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cadastro</title>
+</head>
+
+<body>
+
+    <div class="fundo">
+        <div class="">
+            <h1>Login</h1>
+            <form method="post">
+                <input type="text" name = "inp1" placeholder ="Nome">
+                <input type="text" name = "inp2" placeholder ="Senha"> 
+                <div class="bt">
+                    <input type="submit">
+                </div>
+            </form>
+        </div>
+    </div>
+
+</body>
+</html>
+
+<!-- Email, Nome de usuário, Senha (confirmar senh), Telefone -->
+<!-- Nome de usuário, Senha -->
