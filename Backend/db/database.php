475234052562aa1001a4fe34e7573ff8eb443721
Josué <josuealexandre067@gmail.com>
2025-01-17 14:47:38 -0400

    Update database.php

--- Backend/db/database.php
+++ Backend/db/database.php
@@ -1,31 +1,29 @@
 <?php
 
-class Database
-{
-    private $server = "localhost"; 
-    private $dbnome = "turma31";   
-    private $user = "root";        
-    private $pass = "";           
+    class Database {
+        private $server = "localhost"; 
+        private $dbnome = "Kalangos_Souls";   
+        private $user = "root";        
+        private $pass = "";           
 
-    
-    public function connect(){
-        try {
-            
-            $conn = new PDO(
-                "mysql:host=".$this->server.";dbname=".$this->dbnome, 
-                $this->user, 
-                $this->pass  
-            );
-            
-            $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-            
-            return $conn;
-        } catch (\Exception $e) {
-            
-            echo "Erro no banco de dados: ".$e->getMessage();
+        
+        public function connect() {
+            try {
+                
+                $conn = new PDO(
+                    "mysql:host=".$this->server.";dbname=".$this->dbnome, 
+                    $this->user, 
+                    $this->pass  
+                );
+                
+                $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+                
+                return $conn;
+            } catch (\Exception $e) {
+                
+                echo "Erro: " . $e->getMessage();
+            }
         }
     }
-}
 
-
-
+?>
