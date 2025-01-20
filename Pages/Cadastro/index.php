9b0dc9d4b278acf512b9bf90d9273c97afe6b332
Kayke Adriano <kayke.adrianobdg@gmail.com>
2025-01-20 15:10:00 -0400

    15:09

--- Pages/Cadastro/index.php
+++ Pages/Cadastro/index.php
@@ -1,36 +1,72 @@
 <!DOCTYPE html>
-<html lang="pt-br">
+<html lang="Pt - Br">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Cadastro</title>
 </head>
-
 <body>
+        <div class="fundo">
+                        <h1>Cadastro</h1>
+            <form method = "post">
+                <input type="text" name = "inp1" placeholder = "Nome">
+                <input type="text" name = "inp2" placeholder = "E-mail"> 
+                <input type="text" name = "inp3" placeholder = "Telefone">
+                <input type="text" name = "inp4" placeholder = "Senha"> 
+                <input type="text" name = "inp5" placeholder = "Confirmar Senha"> 
+                <div class = "bt">
+                    <input type="submit">
+                </div>
+            </form>
+        </div>
 
-    <div class="fundo">
-        <h1>Cadastro</h1>
+    <style>
+        * {
+            margin: 0;
+            padding: 0;
+        }
+        
+        body {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            height: 100vh;
+            flex-direction: column;   
+        }
 
-        <form method="POST">
-            <input type="text" name="inp1" placeholder="Nome">
-            <input type="text" name="inp2" placeholder="Email"> 
-            <input type="text" name="inp3" placeholder="Telefone">
-            <input type="text" name="inp4" placeholder="Senha"> 
-            <input type="text" name="inp5" placeholder="Confirmar senha">
+        .fundo {
+            display: flex;
+            background-color: black;
+            justify-content: center;
+            align-items: center;    
+            flex-direction: column;
+            height: 300px;
+            width: 220px;
+        }
 
-            <div class="bt">
-                <input type="submit">
-            </div>
-        </form>
-    </div>
+        h1 {
+            display: flex;
+            color: white;
+            margin-bottom: 9px;
+        }
+
+        input {
+            display: flex;
+            padding: 2px;
+            margin-bottom: 9px;
+        }
+
+        .bt {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+        }
+
+    </style>
+
 
 </body>
 </html>
 
-<!--  -->
-
-<?php
-    
-
-
-?>
+                    <!-- Cadastro -->
+<!-- Email, Nome de usuário, Senha (confirmar senh), Telefone -->
--- Pages/Login/index.php
+++ Pages/Login/index.php
@@ -1,27 +1,70 @@
 <!DOCTYPE html>
-<html lang="pt-BR">
+<html lang="Pt - Br">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Cadastro</title>
+    <title>Login</title>
 </head>
-
 <body>
+        <div class="fundo">
+                        <h1>Login</h1>
+            <form method = "post">
+                <input type="text" name = "inp1" placeholder = "Nome">
+                <input type="text" name = "inp2" placeholder = "Senha"> 
 
-    <div class="fundo">
-        <div class="bloco">
-            <h1>Login</h1>
-
-            <form method="POST">
-                <input type="text" name="inp1" placeholder="Nome">
-                <input type="text" name="inp2" placeholder="Senha">
-                
-                <div class="bt">
+                <div class = "bt">
                     <input type="submit">
                 </div>
             </form>
         </div>
-    </div>
+
+    <style>
+        * {
+            margin: 0;
+            padding: 0;
+        }
+        
+        body {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            height: 100vh;
+            flex-direction: column;   
+        }
+
+        .fundo {
+            display: flex;
+            background-color: black;
+            flex-direction: column;
+            justify-content: center;
+            align-items: center;
+            height: 300px;
+            width: 220px;
+        }
+
+        h1 {
+            display: flex;
+            color: white;
+            margin-bottom: 15px;
+            justify-content: baseline;
+            align-items: center;
+            flex-direction: column;
+        }
+
+        input {
+            display: flex;
+            padding: 2px;
+            margin-bottom: 13px;
+        }
+
+        .bt {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+        }
+
+    </style>
+
 
 </body>
 </html>
