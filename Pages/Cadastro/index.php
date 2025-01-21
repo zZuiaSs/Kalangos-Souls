a948bb8137a6c9920fd9d37d6c9746e10c2f2120
Josué <josuealexandre067@gmail.com>
2025-01-21 15:17:59 -0400

    15:27

--- Pages/Cadastro/index.php
+++ Pages/Cadastro/index.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Cadastro</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
     <link rel="stylesheet" href="style.css">
 </head>
 
@@ -17,11 +18,8 @@
             <input type="text" name="inp2" placeholder="Email"> 
             <input type="text" name="inp3" placeholder="Telefone">
             <input type="text" name="inp4" placeholder="Senha"> 
-            <input type="text" name="inp5" placeholder="Confirmar Senha"> 
-
-            <div class="bt">
-                <input type="submit">
-            </div>
+            <input type="text" name="inp5" placeholder="Confirmar Senha">
+            <button type="button" id="kayke" class="btn btn-outline-success">Enviar</button>
         </form>
     </div>
 
--- Pages/Login/index.php
+++ Pages/Login/index.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
     <link rel="stylesheet" href="style.css">
 </head>
 
@@ -12,13 +13,10 @@
     <div class="fundo">
         <h1>Login</h1>
 
-        <form method="POST" action="">
+        <form class="form" method="POST" action="">
             <input type="text" name="inp1" placeholder="Nome">
             <input type="text" name="inp2" placeholder="Senha"> 
-
-            <div class="bt">
-                <input type="submit">
-            </div>
+            <button type="button" id="kayke" class="btn btn-outline-success">Enviar</button>
         </form>
     </div>
 
