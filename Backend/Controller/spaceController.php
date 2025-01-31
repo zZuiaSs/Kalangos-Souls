44b288d50c083cea018542fd8bf4feba14e4efc1
ZuiaS <josuealexandre067@gmail.com>
2025-01-31 17:55:11 -0400

    17:56

--- Backend/Controller/spaceController.php
+++ Backend/Controller/spaceController.php
@@ -25,15 +25,47 @@
         public function createSpaces($nome, $tipo, $capacidade, $descricao) {
             try {
                 $sql = "INSERT INTO espacos (nome, tipo, capacidade, descricao)
-                        VALUES ()";
+                        VALUES (:nome, :tipo, :capacidade, :descricao)";
                 $db = $this->conn->prepare($sql);
                 $db->bindParam(":nome", $nome);
-                $db;
-
-            } catch(\Exception $th) {
+                $db->bindParam(":tipo", $tipo);
+                $db->bindParam(":capacidade", $capacidade);
+                $db->bindParam(":descricao", $descricao);
+                return $db->execute();
+                
+            } catch (\Exception $th) {
                 return $th->getMessage();
             }
         }
+
+        public function deleteSpaces($id) {
+            try {
+                $sql = "DELETE FROM espacos WHERE id = :id";
+                $db = $this->conn->prepare($sql);
+                $db->bindParam(":id", $id);
+                return $db->execute();
+
+            } catch (\Exception $th) {
+                return $th->getMessage();
+            }
+        }
+
+        public function updateSpaces($nome, $tipo, $capacidade, $descricao) {
+            try {
+                $sql = "UPDATE espacos SET nome = :nome, tipo = :tipo, capacidade = :capacidade, descricao = :descricao WHERE id = :id";
+                $db = $this->conn->prepare($sql);
+                
+                $db->bindParam(":nome", $nome);
+                $db->bindParam(":tipo", $tipo);
+                $db->bindParam(":capacidade", $capacidade);
+                $db->bindParam(":descricao", $descricao);
+                $db->bindParam(":id", $id);
+                return $db->execute();
+
+            } catch (\Exception $th) {
+                return $th->getMessage();
+            }
+        } 
     }
 
 ?>
