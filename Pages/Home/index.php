139ec561ffe0bf388458f26e8ba856c887bc0945
Josué <josuealexandre067@gmail.com>
2025-02-04 17:02:43 -0400

    dfgdfg

--- Pages/Home/index.php
+++ Pages/Home/index.php
@@ -27,6 +27,26 @@
 
 <body>
 
+<<<<<<< Updated upstream
+=======
+  <!-- S C R I P T -->
+
+  <div class="fundo">
+    <form action="../../Backend/Router/userRouter.php">
+      <div class="profile-img2">
+        <h1 style="font-size: 50px; color: greenyellow;" id="primeira-letra2"><?php echo $usuario[0] ?></h1>
+      </div>
+
+      <input name="nome" type="text" value="<?php  ?>">
+      <input name="email" type="text" value="<?php  ?>">
+      <input name="senha" type="text" value="<?php  ?>">
+      <input name="telefone" type="text" value="<?php  ?>">
+
+      <button ></button>
+    </form>
+  </div>
+
+>>>>>>> Stashed changes
   <!-- C O N T E Ú D O -->
 
   <div class="container">
@@ -44,7 +64,6 @@
           <div class="menu">
             <button class="menu-item"><svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#FFFFFF"><path d="M580-240q-42 0-71-29t-29-71q0-42 29-71t71-29q42 0 71 29t29 71q0 42-29 71t-71 29ZM200-80q-33 0-56.5-23.5T120-160v-560q0-33 23.5-56.5T200-800h40v-80h80v80h320v-80h80v80h40q33 0 56.5 23.5T840-720v560q0 33-23.5 56.5T760-80H200Zm0-80h560v-400H200v400Zm0-480h560v-80H200v80Zm0 0v-80 80Z"/></svg>Minhas reservas</button>
             <button class="menu-item"><svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#FFFFFF"><path d="M480-240Zm-320 80v-112q0-34 17.5-62.5T224-378q62-31 126-46.5T480-440q37 0 73 4.5t72 14.5l-67 68q-20-3-39-5t-39-2q-56 0-111 13.5T260-306q-9 5-14.5 14t-5.5 20v32h240v80H160Zm400 40v-123l221-220q9-9 20-13t22-4q12 0 23 4.5t20 13.5l37 37q8 9 12.5 20t4.5 22q0 11-4 22.5T903-340L683-120H560Zm300-263-37-37 37 37ZM620-180h38l121-122-18-19-19-18-122 121v38Zm141-141-19-18 37 37-18-19ZM480-480q-66 0-113-47t-47-113q0-66 47-113t113-47q66 0 113 47t47 113q0 66-47 113t-113 47Zm0-80q33 0 56.5-23.5T560-640q0-33-23.5-56.5T480-720q-33 0-56.5 23.5T400-640q0 33 23.5 56.5T480-560Zm0-80Z"/></svg>Editar perfil</button>
-            <button class="menu-item"><svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#FFFFFF"><path d="M444-200h70v-50q50-9 86-39t36-89q0-42-24-77t-96-61q-60-20-83-35t-23-41q0-26 18.5-41t53.5-15q32 0 50 15.5t26 38.5l64-26q-11-35-40.5-61T516-710v-50h-70v50q-50 11-78 44t-28 74q0 47 27.5 76t86.5 50q63 23 87.5 41t24.5 47q0 33-23.5 48.5T486-314q-33 0-58.5-20.5T390-396l-66 26q14 48 43.5 77.5T444-252v52Zm36 120q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-80q134 0 227-93t93-227q0-134-93-227t-227-93q-134 0-227 93t-93 227q0 134 93 227t227 93Zm0-320Z"/></svg>Métodos de pagamento</button>
           </div>
         </div>
 
@@ -62,11 +81,24 @@
           </div>
           Cadastrar espaço
         </button>
-        <button class="topbar-btn orange">
-          <div style="opacity: 0.25; display: flex; align-items:center; justify-content:center; background-color: #1A1A1A; padding: 10px; border-radius: 100%;">
+<<<<<<< Updated upstream
+=======
+        <button class="topbar-btn blue">
+          <div style="display: flex; align-items:center; justify-content:center; background-color: #1A1A1A; padding: 10px; border-radius: 100%;">
             <svg xmlns="http://www.w3.org/2000/svg" height="30px" viewBox="0 -960 960 960" width="30px" fill="#FFFFFF"><path d="M440-440H200v-80h240v-240h80v240h240v80H520v240h-80v-240Z"/></svg>
           </div>
+          Meus espaços
+        </button>
+>>>>>>> Stashed changes
+        <button class="topbar-btn orange">
+          <div style="display: flex; align-items:center; justify-content:center; background-color: #1A1A1A; padding: 10px; border-radius: 100%;">
+            <svg xmlns="http://www.w3.org/2000/svg" height="30px" viewBox="0 -960 960 960" width="30px" fill="#FFFFFF"><path d="M440-440H200v-80h240v-240h80v240h240v80H520v240h-80v-240Z"/></svg>
+          </div>
+<<<<<<< Updated upstream
           Listar Usuários
+=======
+          Usuários
+>>>>>>> Stashed changes
         </button>
       </header>
 
@@ -93,11 +125,16 @@
             <img class="card-img" src="https://cdn0.casamentos.com.br/vendor/7952/3_2/960/jpg/16125676385173_13_307952-161290249528500.jpeg" alt="img do card">
             <h3 class="card-title">Espaço Canto Belo</h3>
              <p class="card-subtitle">Espaço de festas com piscina e churrasqueira</p>
+<<<<<<< Updated upstream
             <p class = "card-subtitle">Capacidade: 50</p>
             <button id="reservar">Reservar</button>
           </div>  
             
 
+=======
+            <p class = "capacidade">Capacidade: 50></p>
+          </div>
+>>>>>>> Stashed changes
         </main>
       </div>
     </div>
