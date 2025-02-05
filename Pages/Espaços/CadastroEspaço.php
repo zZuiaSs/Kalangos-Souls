23f04ba8eaf6b018cdfa10d5e5e965bd72ebe4ce
ZuiaS <josuealexandre067@gmail.com>
2025-02-04 21:52:55 -0400

    isso

--- Pages/Espaços/CadastroEspaço.php
+++ Pages/Espaços/CadastroEspaço.php
@@ -1,9 +1,7 @@
 <?php
-session_start();
-if (!isset($_SESSION["id_usuario"])) {
-    header('Location: ../../index.php');
-    exit();
-}
+
+
+
 ?>
 
 <!DOCTYPE html>
@@ -18,17 +16,10 @@ if (!isset($_SESSION["id_usuario"])) {
 <div class="container">
     <h2>Cadastrar Espaço</h2>
     <form action="../../Backend/Router/spaceRouter.php?acao=cadastrar" method="POST">
-        <label for="nome">Nome do Espaço:</label>
-        <input type="text" id="nome" name="nome" required>
-
-        <label for="tipo">Tipo de Espaço:</label>
-        <input type="text" id="tipo" name="tipo" required>
-
-        <label for="capacidade">Capacidade:</label>
-        <input type="number" id="capacidade" name="capacidade" required>
-
-        <label for="descricao">Descrição:</label>
-        <textarea id="descricao" name="descricao" required></textarea>
+        <input type="text" id="nome" name="nome" placeholder="Nome do espaço" required>
+        <input type="text" id="tipo" name="tipo" placeholder="Tipo de espaço" required>
+        <input type="number" id="capacidade" name="capacidade" placeholder="Capacidade" required>
+        <textarea id="descricao" name="descricao" placeholder="Descrição" required></textarea>
 
         <button type="submit" class="button">Cadastrar</button>
     </form>
